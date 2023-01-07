b629b15b8ab21b9f176a5a95196aee38d0ce2694
NoahELE <cxhmel@outlook.com>
2023-01-07 13:32:53 +0800

    add manga list page in web ui

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard',
+    'standard-jsx',
+    'standard-react',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+}
---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../src/main/resources/static',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom', 'zustand'],
+          antd: ['antd'],
+          lodash: ['lodash-es'],
+          axios: ['axios'],
+        },
+      },
+    },
+  },
+})
